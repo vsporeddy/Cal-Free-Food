2e4d697fc010318fe7c6c9590d3e16b4b778a0e4
Ronald Arnanjaya <Onel@airbears2-10-142-34-25.airbears2.1918.berkeley.edu>
2014-10-18 15:58:43 -0700

    i

--- free_food.py
+++ free_food.py
@@ -5,13 +5,13 @@ import gflags
 import base64
 import email
 
-#from mercurial import Lib/quopri
-from bs4 import BeautifulSoup
-from apiclient.discovery import build
+from googleapiclient.discovery import build
+#from googleapiclient import discovery
+#from discovery import build
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run
-from apiclient import errors
+from googleapiclient import errors
 
 
 # Path to the client_secret.json file downloaded from the Developer Console
@@ -54,11 +54,10 @@ def get_msg(query, service = gmail_service, user_id = 'me'):
 
     return messages
   except errors.HttpError, error:
-    print 'An error occurred: %s' % error
+    print('An error occurred:' + error)
 
 #free_msg = get_msg("free food is:unread"), 
-food_msg = get_msg("food OR snacks OR lunch OR dinner OR drink OR pizza OR barbecue")
-
+food_msg = get_msg("food OR lunch OR dinner OR drink OR pizza OR barbecue")
 
 #print "Containing 'free':"
 #for i in free_msg:
@@ -70,18 +69,29 @@ food_msg = get_msg("food OR snacks OR lunch OR dinner OR drink OR pizza OR barbe
 #  message = gmail_service.users().messages().get(userId='me', id=i['id'], format='raw').execute()
 #  print '%s...' % message['snippet']
 
-print " "
-print "Containing 'free food'"
+print(" ")
+print("Containing 'free food'")
 for i in food_msg:
     message = gmail_service.users().messages().get(userId='me', id=i['id'], format='raw').execute()
-    filename = 'platter/' + message['snippet'] + '.txt'
+    snip = message['snippet']
+    count = 0
+    start, b = 0, False
+    for c in snip:
+        if c == "!" or c == "." or c == "?" or (c=="&" and b):
+            snip=snip[start:count]
+        if c == "&" and not b:
+            start = count+6
+            b=True
+        count+=1
+    print(snip)
+    filename = 'platter/' + snip + '.txt'
     msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
-
-
+    
+    
     msg = email.message_from_string(msg_str)
     f = open(filename, "a")
     f.write(msg_str)
-    #msg = email.message_from_string(msg_str)
-    #print msg_str
-    #soup = BeautifulSoup(msg)
-    #print soup.get_text()
+#msg = email.message_from_string(msg_str)
+#print msg_str
+#soup = BeautifulSoup(msg)
+#print soup.get_text()
