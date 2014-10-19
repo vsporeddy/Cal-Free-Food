f412ed8a741ee0bef47a33585300f7431aec4a3f
Sirblobfish <s.utkarsh@berkeley.edu>
2014-10-18 17:52:34 -0700

    Added exceprt extraction

--- regex_test.py
+++ regex_test.py
@@ -22,14 +22,16 @@ def regex(string):
 	start_date = 18
 	end_date = 20
 	start_month = 'October'
-	search_result =  re.findall('Today|Tomorrow|today|tomorrow', string) + re.findall('Oct 18|October 18|Oct 19|October 19|Oct 20|October 20|18th October|19th October|20th October', string) # re.findall(start_month[:3]+'|'+start_month+' ['+str(start_date)+'-'+str(end_date) +']', string) + re.findall('['+str(start_date)+'-'+str(end_date) +'] '+start_month[:3]+'|'+start_month+'', string) + re.findall('['+str(start_date)+'-'+str(end_date) +']th Oct|'+start_month+'', string)
+	search_result =  re.findall('..............................Today..............................|..............................Tomorrow..............................|..............................today..............................|..............................tomorrow..............................', string) + re.findall('..............................Oct 18..............................|..............................October 18..............................|..............................Oct 19..............................|..............................October 19..............................|..............................Oct 20..............................|..............................October 20..............................|..............................18th October..............................|..............................19th October..............................|..............................20th October..............................', string) # re.findall(start_month[:3]+'|'+start_month+' ['+str(start_date)+'-'+str(end_date) +']', string) + re.findall('['+str(start_date)+'-'+str(end_date) +'] '+start_month[:3]+'|'+start_month+'', string) + re.findall('['+str(start_date)+'-'+str(end_date) +']th Oct|'+start_month+'', string)
 	return search_result
 
 for email in email_list:
 	reg=regex(content(email))
 	if len(reg)>0:
 		t = open(file_address(email), 'w+')
-		t.write( file_name(email) + "\n\n" + content(email) +"\n\n\nTags:"+str(reg) )
+		t.write( file_name(email) + "\nExcerpt:"+ str(reg)+ "\n\n" content(email) )
 	else:
 		file_object(email).close()
 		os.remove(file_address(email))
+
+..............................
